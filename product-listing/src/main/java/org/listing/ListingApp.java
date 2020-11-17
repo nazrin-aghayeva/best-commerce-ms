36dfc4bc588775abea50f93a8130a066b82ea4c9
nazrin-aghayeva <nazrinaghayeva@gmail.com>
2020-11-17 17:50:48 +0400

    -- product listing

---
+++ product-listing/src/main/java/org/listing/ListingApp.java
@@ -0,0 +1,11 @@
+package org.listing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ListingApp.class, args);
+    }
+}
